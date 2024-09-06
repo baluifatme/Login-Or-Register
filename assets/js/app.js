a6e6ce24bbbd9b01f0b69b1585c24f08fff9c389
mohammad yousefnia <baluifateme@gmail.com>
2024-09-06 06:43:51 +0330

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+  const acc1 = {
+    username: "kosarNouri",
+    password: "kosar1378",
+  };
+  const acc2 = {
+    username: "sadaftabari",
+    password: "sadaf1378",
+  };
+
+  const acc3 = {
+    username: "kianajavadpur",
+    password: "kiana1378",
+  };
+
+  const accounts = [acc1, acc2, acc3];
+
+  const userInput = $("#userName");
+  const passInput = $("#password");
+  const signInBtn = $("#signinbtn");
+  const userValidation = $(".user-validation");
+  userValidation.empty();
+
+  signInBtn.on("click", (e) => {
+    let interUser = userInput.val();
+    let interPass = passInput.val();
+    const matchingAcc = accounts.find((acc) => {
+      return acc.username === interUser && acc.password === interPass;
+    });
+    if (matchingAcc) {
+      errorSet("✔your info is correct");
+      $(".user-validation").css({ color: "green" });
+    } else if (interUser === "" || interPass === "") {
+      errorSet("*please fill the fields");
+      $(".user-validation").css({ color: "red" });
+    } else {
+      errorSet("*please enter the correct info");
+      $(".user-validation").css({ color: "red" });
+
+      userInput.val("");
+      passInput.val("");
+    }
+  });
+
+  const errorSet = function (errortext) {
+    userValidation.text(errortext);
+  };
+});
